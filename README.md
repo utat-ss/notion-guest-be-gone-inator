9beb049ec91fb78da12bcb0c79925d7c83e0df17
David Maranto <34867698+DM1122@users.noreply.github.com>
2023-04-30 01:54:22 +0000

    Add commandline parsing, csv reading

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Notion GuestBeGoneInator
 A simple script to mass purge guest access from a Notion page
+
+# Usage
+
+
+```
+cargo run -- path/to/email/list.csv
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{App, Arg};
+use csv;
+use notion::ids::PageId;
+use notion::NotionApi;
+use std::error::Error;
+use std::path::Path;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let matches = App::new("My Notion Program")
+        .version("1.0")
+        .author("Your Name <your.email@example.com>")
+        .about("A program to remove guest access to a Notion page")
+        .arg(
+            Arg::with_name("API_KEY")
+                .help("API key for Notion API")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("PAGE_ID")
+                .help("ID of the Notion page")
+                .required(true)
+                .index(2),
+        )
+        .arg(
+            Arg::with_name("EMAIL_LIST")
+                .help("Path to the CSV file containing email list")
+                .required(true)
+                .index(3),
+        )
+        .get_matches();
+
+    let api_key = matches.value_of("API_KEY").unwrap();
+    let page_id = matches.value_of("PAGE_ID").unwrap();
+    let email_list_path = Path::new(matches.value_of("EMAIL_LIST").unwrap());
+
+    let client = NotionApi::new(api_key);
+
+    let reader = csv::Reader::from_path(email_list_path)?;
+    for result in reader.into_records() {
+        let record = result?;
+        let email = &record[0];
+        println!("Email: {}", email);
+    }
+
+    Ok(())
 }
+
+// fn revoke_guest_access(client: NotionAPI, page_id: &PageId, user_id: &str) {
+//     let page = client.get_page(page_id);
+//     let mut properties = page.properties.clone();
+
+//     if let Some(permissions) = properties.page_user_permissions_mut() {
+//         permissions.retain(|permission| permission.user_id() != user_id);
+//     }
+
+//     client.update_page_properties(page_id, properties);
+// }
