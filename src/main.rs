61ccbd30f55bd7c13d28e524d6ff65f669ba9c16
David Maranto <34867698+DM1122@users.noreply.github.com>
2023-05-08 19:43:53 +0000

    Move api key to env

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use clap::{arg, command};
+use dotenv::dotenv;
+use futures::executor::block_on;
 use notion::ids::PageId;
 use notion::NotionApi;
 use serde::Deserialize;
+use std::env;
 use std::error::Error;
 use std::path::Path;
 use std::str::FromStr;
-use futures::executor::block_on;
 
 #[derive(Debug, Deserialize)]
 struct EmailRecord {
@@ -18,24 +20,22 @@ struct EmailRecord {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let matches = command!()
-        .arg(arg!([API_KEY] "API key for Notion API").required(true))
-        .arg(arg!([PAGE_ID] "ID of the Notion page").required(true))
+        .arg(arg!([PAGE_ID] "ID of the Notion page from which to remove guest permissions. This will be the root page of a Notion workspace in most cases").required(true))
         .arg(arg!([EMAIL_LIST] "Path to the CSV file containing email list").required(true))
         .get_matches();
-    let api_key = matches
-        .get_one::<String>("API_KEY")
-        .expect("API_KEY is required");
     let page_id = PageId::from_str(
         matches
             .get_one::<String>("PAGE_ID")
             .expect("PAGE_ID is required"),
     )
     .expect("Page id should be parsable");
-    // let page_id = page_id_str
     let email_list = matches
         .get_one::<String>("EMAIL_LIST")
         .expect("EMAIL_LIST is required");
     let email_list_path = Path::new(&email_list);
+
+    dotenv().ok();
+    let api_key = env::var("API_KEY").expect("API_KEY not found in .env");
 
     let client = NotionApi::new(api_key.to_string())?;
 
@@ -51,6 +51,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn revoke_guest_access(client: &NotionApi, page_id: &PageId, user_id: &str) {
-    let page = block_on(client.get_page(page_id));
+    let page = block_on(client.get_page(page_id)).expect("Page should be addressable");
+    let properties = page.properties;
+    println!("{properties:#?}");
 }
-
